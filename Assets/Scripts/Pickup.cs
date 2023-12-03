cbad3bd85466d6f88aebb2ba2be3bc6089f553a6
Caroline Jareb <cjareb@gmail.com>
2023-12-03 17:47:36 +0100

    Created pickup script

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour, IInteractable
+{
+
+    private bool isHolding;
+    private Rigidbody rgb;
+    public Transform playerCamera;
+
+    void Start()
+    {
+        isHolding = false;
+        rgb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Interact()
+    {
+        if (isHolding == true)
+        {
+            PutDown();
+        }
+        else
+        {
+            PickUp();
+        }
+
+    }
+
+    public void PickUp()
+    {
+        transform.SetParent(playerCamera);
+        rgb.isKinematic = true;
+        isHolding = true;
+    }
+
+    public void PutDown()
+    {
+        transform.parent = null;
+        rgb.isKinematic = false;
+        isHolding = false;
+    }
+
+    public string GetDescription()
+    {
+        if (isHolding == true)
+        {
+            return "Put down";
+        }
+        else
+        {
+            return "Pick up";
+        }
+    }
+}
