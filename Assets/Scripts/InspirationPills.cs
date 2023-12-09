9c35a1c6bffc885bf2450d4d3837c3f9919f49a2
Caroline Jareb <cjareb@gmail.com>
2023-12-09 16:39:51 +0100

    inspiration pills prefab made

---
+++ Assets/Scripts/InspirationPills.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InspirationPills : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        //effects
+    }
+
+    public string GetDescription()
+    {
+        return "Take Inspiration Pills";
+    }
+}
