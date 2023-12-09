45a77ea6cd471db47be9658f83bd2373692a7932
joshphythian <joshphythian1@gmail.com>
2023-12-09 21:19:45 +0100

    Finished the letters

--- Assets/Scripts/Pill/InspirationPills.cs
+++ Assets/Scripts/Pill/InspirationPills.cs
@@ -2,11 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class InspirationPills : MonoBehaviour, IInteractable
 {
+    public GameObject Gchord;
+    public GameObject BflatChord;
+    public GameObject AminChord;
+    public GameObject FmajChord;
+
+    public void Start()
+    {
+
+    }
+
     public void Interact()
     {
-        //effects
+        postProcessingEnable.instance.TurnOnInspiration();
+        Gchord.GetComponent<TMPro.TMP_Text>().color = Color.red;
+        BflatChord.GetComponent<TMPro.TMP_Text>().color = Color.red;
+        AminChord.GetComponent<TMPro.TMP_Text>().color = Color.red;
+        FmajChord.GetComponent<TMPro.TMP_Text>().color = Color.red;
     }
 
     public string GetDescription()
